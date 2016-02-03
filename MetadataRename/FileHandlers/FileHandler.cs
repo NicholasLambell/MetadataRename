106893b4afa2ba60a5573e33c1414445740600d6
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-02-04 01:10:56 +1000

    Changed / removed methods to suit the new application

--- MetadataRename/FileHandlers/FileHandler.cs
+++ MetadataRename/FileHandlers/FileHandler.cs
@@ -8,60 +8,20 @@ using System.Threading.Tasks;
 
 namespace MetadataRename {
     class FileHandler {
-        //public static bool Rename(FileInfo file, string rename) {
-        //    if (file != null && file.Exists && !String.IsNullOrEmpty(rename)) {
-        //        int iteration = 1;
-        //        string fileName = Util.FileName(file.Name);
-
-        //        //Try to get iteration from file name if it's at least 4 chars long
-        //        if (fileName != null && fileName.Length >= 4) {
-        //            int iterationGet = GetIteration(fileName);
-        //            iteration = (iterationGet > iteration) ? iterationGet : iteration;
-        //        }
-
-        //        //If the rename string contains the iteration patern eg " (2)" remove it.
-        //        if (iteration >= 2) {
-        //            Regex regex = new Regex(@" \(\d+\)$");
-        //            if (regex.IsMatch(rename)) {
-        //                rename = regex.Replace(rename, "");
-        //            }
-        //        }
-
-        //        //Loop through iterations trying to rename until a value one is found
-        //        while (!ApplyRename(file, rename, iteration)) {
-        //            iteration++;
-        //        }
-        //        return true;
-        //    }
-        //    return false;
-        //}
-        
-        //private static bool ApplyRename(FileInfo file, string rename, int iteration) {
-        //    if (file != null && file.Exists && !String.IsNullOrEmpty(rename)) {
-        //        try {
-        //            //Check iteration and form iteration string
-        //            string sIteration = (iteration >= 2) ? String.Format(" ({0})", iteration) : "";
-                    
-        //            //Try to rename the file
-        //            file.MoveTo(file.Directory.FullName + @"\" + rename + sIteration + file.Extension);
-        //            return true;
-
-        //        } catch (IOException) { //Triggered if file exists, goes back to while loop above
-        //        } catch (Exception ex) {
-        //            ErrorHandler.LogError(ex.GetType() + ": " + ex.Message, true);
-        //        }
-        //    }
-        //    return false;
-        //}
-
-        //private static int GetIteration(string fileName) {
-        //    //If the last characters of the string are "(number)" extract the number
-        //    Regex regex = new Regex(@"(?<=\()\d+(?=\)$)");
-        //    Match match = regex.Match(fileName);
-
-        //    //Return the extracted number or if empty or not a number return 0
-        //    int output;
-        //    return (match.Groups[0].Value != String.Empty && Int32.TryParse(match.Groups[0].Value, out output)) ? output : 0;
-        //}
+        public static bool ApplyTags(List<FileInfo> files) {
+            try {
+                if (files != null && files.Count > 0) {
+                    foreach (FileInfo file in files) {
+                        TagLib.File tagFile = TagLib.File.Create(file.FullName);
+                        tagFile.Tag.Title = Util.FileName(file.Name);
+                        tagFile.Save();
+                    }
+                }
+                return true;
+            } catch (Exception ex) {
+                ErrorHandler.LogError(ex.GetType() + ":\n" + ex.Message, false);
+            }
+            return false;
+        }
     }
 }
